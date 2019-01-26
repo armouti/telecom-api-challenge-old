0bff13c71f91f8e6c16310f42b848d25d8ce28fb
Ismail Alarmouti <alarmouti.ismail@gmail.com>
2019-01-26 19:34:40 +0000

    Created unit tests for the PhoneNumber DB model

---
+++ tests/database/PhoneNumber.test.js
@@ -0,0 +1,27 @@
+const PhoneNumberTable = require('../../database/PhoneNumberTable.js');
+
+describe('PhoneNumber DB Model', () => {
+  const phoneNumberTable = new PhoneNumberTable();
+  const testNumber = '07583749326';
+  const testNumber2 = '07553745321'
+  const testId = 100;
+
+  test('Adds a new phone number', () => {
+    phoneNumberTable.addNumber(testNumber, testId, false);
+    expect(phoneNumberTable.getNumberInfo(testNumber)).toBeTruthy();
+  });
+
+  test('Activates a phone number', () => {
+    phoneNumberTable.activate(testNumber);
+    const isActive = phoneNumberTable.getNumberInfo(testNumber).isActive;
+    expect(isActive).toBeTruthy();
+  });
+
+  test('Retrieves all numbers by customer_id', () => {
+    //adding another number
+    phoneNumberTable.addNumber(testNumber2, testId);
+    
+    const num = phoneNumberTable.getNumbersByCID(testId);
+    expect(num).toEqual([testNumber, testNumber2]);
+  });
+})
